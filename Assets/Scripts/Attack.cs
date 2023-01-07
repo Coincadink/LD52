dab9aef89922825869995209231524cee1f5146e
Tim Lawrence <timothyjl2002@gmail.com>
2023-01-07 14:42:22 -0700

    Basic attack system

---
+++ Assets/Scripts/Attack.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    public int Lifetime;
+    private int age;
+
+    public int Damage;
+    public GameObject Owner;
+
+    public void Start()
+    {
+        age = 0;
+    }
+
+    public void FixedUpdate()
+    {
+        age++;
+        if (age > Lifetime)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject != Owner)
+        {
+            collision.gameObject.GetComponent<Entity>().Damage(Damage);
+        }
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,22 +1,34 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Movement : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     public float speed = 0.15f;
     private bool facingRight = true;
 
+    public GameObject Attack;
+
     public void FixedUpdate()
     {
-        Vector3 translation = new Vector3();
-        translation.x = Input.GetAxis("Horizontal") * speed;
-        translation.y = Input.GetAxis("Vertical") * speed;
+        Vector3 translation = new()
+        {
+            x = Input.GetAxis("Horizontal") * speed,
+            y = Input.GetAxis("Vertical") * speed
+        };
 
         transform.position += translation;
 
         if (translation.x < 0 && facingRight || translation.x > 0 && !facingRight) 
             Flip();
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            var newAtt = Instantiate(Attack, transform.position, transform.rotation);
+            newAtt.GetComponent<Attack>().Owner = gameObject;
+        }
     }
 
     private void Flip()
