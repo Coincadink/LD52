f1c794286d75a96b5f59716eb12b06e1e67c4b55
Tim Lawrence <timothyjl2002@gmail.com>
2023-01-07 14:42:22 -0700

    Basic attack system

---
+++ Assets/Scripts/Entity.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entity : MonoBehaviour
+{
+    public int MaxHealth;
+    private int health;
+
+    void Start()
+    {
+        health = MaxHealth;
+    }
+
+    public void Damage(int damage)
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
