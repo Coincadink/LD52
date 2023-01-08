1642f94c5d4d012a7d8ef53a2ad3d4a3c7c838eb
Tim Lawrence <timothyjl2002@gmail.com>
2023-01-08 00:35:57 -0700

    HE TWEAKIN

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float Speed;
+
+    private Rigidbody2D m_rigidbody;
+
+    private void Start()
+    {
+        m_rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 direction = new Vector2
+        {
+            x = Random.value,
+            y = Random.value
+        }.normalized;
+
+        m_rigidbody.MovePosition(direction * Speed);
+    }
+}
